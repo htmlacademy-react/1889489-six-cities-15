793688784fa1d390f8511e9c50a8fdbf0f0748ff
Anton <anton.mogilev@mail.ru>
2024-02-25 16:43:29 +0300

    Добавит компонент Logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,7 @@ function Header(): JSX.Element {
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <a className="header__logo-link header__logo-link--active">
+            <a className="header__logo-link" href="main.html">
               <img
                 className="header__logo"
                 src="img/logo.svg"
--- src/pages/page-main/page-main.tsx
+++ src/pages/page-main/page-main.tsx
@@ -1,5 +1,4 @@
 import CitiesCard from '../../components/cities-card/cities-card';
-import Header from '../../components/header/header';
 
 type PageMainProps = {
   citiesPlacesCount: number;
@@ -8,7 +7,44 @@ type PageMainProps = {
 function PageMain({citiesPlacesCount}: PageMainProps): JSX.Element {
   return (
     <div className="page page--gray page--main">
-      <Header />
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link header__logo-link--active">
+                <img
+                  className="header__logo"
+                  src="img/logo.svg"
+                  alt="6 cities logo"
+                  width={81}
+                  height={41}
+                />
+              </a>
+            </div>
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                <li className="header__nav-item user">
+                  <a
+                    className="header__nav-link header__nav-link--profile"
+                    href="#"
+                  >
+                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                    <span className="header__user-name user__name">
+                  Oliver.conner@gmail.com
+                    </span>
+                    <span className="header__favorite-count">3</span>
+                  </a>
+                </li>
+                <li className="header__nav-item">
+                  <a className="header__nav-link" href="#">
+                    <span className="header__signout">Sign out</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
