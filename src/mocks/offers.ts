4cbb4db519e9a18aa13ddba37748c807a37d3885
Anton <anton.mogilev@mail.ru>
2024-02-29 20:23:50 +0300

    Добавит моки для offers.

---
+++ src/mocks/offers.ts
@@ -0,0 +1,96 @@
+import { Offer } from '../types/offer';
+
+export const offers: Offer[] = [
+  {
+    'id': 'f5d95974-342e-4837-be2d-814ead90908e',
+    'title': 'The Pondhouse - A Magical Place',
+    'type': 'house',
+    'price': 202,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/9.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.868610000000004,
+      'longitude': 2.342499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 1.9
+  },
+  {
+    'id': '53333b49-291e-4338-a23a-f514e86ec7c8',
+    'title': 'Beautiful & luxurious apartment at great location',
+    'type': 'hotel',
+    'price': 300,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/13.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.858610000000006,
+      'longitude': 2.330499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': true,
+    'rating': 2
+  },
+  {
+    'id': 'e8c444f7-241a-478e-aa12-be04824d8071',
+    'title': 'Penthouse, 4-5 rooms + 5 balconies',
+    'type': 'apartment',
+    'price': 334,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/10.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.834610000000005,
+      'longitude': 2.335499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': true,
+    'rating': 1.2
+  },
+  {
+    'id': 'c3f5cdc1-a8da-4a85-a176-6ff87fed6b96',
+    'title': 'Perfectly located Castro',
+    'type': 'house',
+    'price': 431,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/11.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.85761,
+      'longitude': 2.358499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 2.3
+  }
+];
