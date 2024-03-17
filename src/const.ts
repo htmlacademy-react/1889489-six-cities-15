b3633e41347797ca28f3c0798d738a28d108dce2
Anton <anton.mogilev@mail.ru>
2024-03-17 16:02:52 +0300

    Подключит подсветку маркеров на карте

--- src/const.ts
+++ src/const.ts
@@ -23,8 +23,6 @@ export const MIN_REVIEW_LENGTH = 50;
 
 export const MAX_REVIEW_LENGTH = 300;
 
-export const URL_MARKER_DEFAULT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+export const URL_MARKER_DEFAULT = 'img/pin.svg';
 
-export const URL_MARKER_CURRENT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+export const URL_MARKER_CURRENT = 'img/pin-active.svg';
