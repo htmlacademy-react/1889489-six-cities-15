76d9c4320f74865e63604ce77fb82af3eca83d98
Anton <anton.mogilev@mail.ru>
2024-03-21 00:23:37 +0300

    Добавит новое действие загрузки офферов.

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,24 +1,25 @@
 import {createReducer} from '@reduxjs/toolkit';
 import { changeCity, changeSortingType, loadOffers } from './action';
 import { SixCities } from '../types/sixCities';
-import { offers } from '../mocks/offers';
+// import { offers } from '../mocks/offers';
 import { TypesOfSorting } from '../const';
+import { Offer } from '../types/offer';
 
 const INITIAL_CITY: SixCities = 'Paris';
 
 const initialState = {
   city: INITIAL_CITY as SixCities,
-  offers: offers,
+  offers: [] as Offer[],
   sortingType: TypesOfSorting.Popular,
 };
 
 const reducer = createReducer(initialState, (builder) => {
   builder
-    .addCase(changeCity, (state, action) => {
-      state.city = action.payload;
-    })
     .addCase(loadOffers, (state, action) => {
       state.offers = action.payload;
+    })
+    .addCase(changeCity, (state, action) => {
+      state.city = action.payload;
     })
     .addCase(changeSortingType, (state, action) => {
       state.sortingType = action.payload;
