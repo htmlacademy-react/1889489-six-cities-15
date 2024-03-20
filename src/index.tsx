66a33a8d6849760386a268a5d49bd4fea195fd0c
Anton <anton.mogilev@mail.ru>
2024-03-21 01:24:57 +0300

    Добавит обработку ошибок взаимодействия с сервером

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import { offers } from './mocks/offers';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import ErrorMessage from './components/error-message/error-message';
+import { checkAuthAction } from './store/api-actions';
+
+store.dispatch(checkAuthAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
